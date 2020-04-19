f145c6917a0296447dae4cacfcab41610c456e44
Heather Moreira <moreiraheather@gmail.com>
2020-04-19 20:40:27 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -39,6 +39,19 @@ class TicTacToe
   def valid_move?(index)
     index.between?(0,8) && !position_taken?(index)
   end 
+  
+  def turn
+  puts "Please choose a number 1-9:"
+  user_input = gets.chomp
+  index = input_to_index(user_input)
+  if valid_move?(index)
+   token = current_player
+    move(index, token)
+    display_board
+  else
+    turn
+  end
+end
 
   # def turn
   #   puts "Please enter 1-9:"
